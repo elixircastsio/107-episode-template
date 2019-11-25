e70b9091d55fd6e10edb9fb6cfddc95ccd7148cf
Alekx <alekx.lang@gmail.com>
2019-11-24 19:56:10 -0700

    init commit

---
+++ lib/teacher_web/router.ex
@@ -0,0 +1,28 @@
+defmodule TeacherWeb.Router do
+  use TeacherWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug TeacherWeb.Plugs.SetAuth
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", TeacherWeb do
+    pipe_through :browser
+
+    resources "/sessions", SessionController, only: [:new, :create]
+    delete "/sign-out", SessionController, :delete
+    resources "/registrations", RegistrationController, only: [:new, :create]
+
+    resources "/albums", AlbumController
+    resources "/users", UserController, only: [:show]
+    get "/", PageController, :index
+  end
+end
---
+++ lib/teacher_web/views/album_view.ex
@@ -0,0 +1,5 @@
+defmodule TeacherWeb.AlbumView do
+  use TeacherWeb, :view
+  import Scrivener.HTML
+
+end
